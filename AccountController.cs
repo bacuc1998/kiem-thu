7c1c6f4e3f4c58ae2595217f969174148410cb7a
bacuc1998 <74974062+bacuc1998@users.noreply.github.com>
2022-01-08 22:55:20 +0700

    Controller account

---
+++ AccountController.cs
@@ -0,0 +1,24 @@
+﻿using LibraryManage.DatabaseAccess;
+using LibraryManage.Models;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace LibraryManage.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            LibraryDBEntities db = new LibraryDBEntities();
+            var account = db.Accounts.ToList();
+
+            return View(account);
+        }
+    }
+}
